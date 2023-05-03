2304de99cc06c04484b2f454e11fff0e1cc4e41a
celthi <evan_l00@qq.com>
2023-05-03 18:14:41 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version=2.20.1
-cargo check && docker build . -t webhook-gateway:$version
+cargo check && docker build . -t webhook_gateway:$version
 sleep 1
-kubectl set image  deployment/webhook-gateway webhook-gateway=webhook-gateway:$version
+kubectl set image  deployment/webhook-gateway webhook-gateway=webhook_gateway:$version
