ef9e6d9ba73e268a627233b1504a673fa0363677
celthi <evan_l00@qq.com>
2023-05-03 23:49:50 +0800

    refactor

--- src/events/github/handler.rs
+++ src/events/github/handler.rs
@@ -1,6 +1,7 @@
 use crate::constants;
 use crate::events::github::event::GithubEvent;
-use crate::events;
+use crate::events::msg::task;
+use crate::events::msg::Message;
 use crate::events::msg::time_spent;
 use crate::events::msg::time_spent::TimeSpentTrait;
 use crate::events::msg::queue;
@@ -25,13 +26,13 @@ pub fn handle_github_event(event: &GithubEvent) -> Result<()> {
 
 fn handle_ocr_event(event: &GithubEvent, comment: &str) -> Result<(), anyhow::Error> {
     if let (Some(repo), Some(pr)) = (event.get_repo_name(), event.get_pr_number()) {
-        let task = events::msg::task::get_task_from_str(
+        let task = task::get_task_from_str(
             comment,
             repo.to_string(),
             pr,
             event.get_user_name(),
         )?;
-        let msg = events::msg::Message::Task(task);
+        let msg = Message::Task(task);
         let s = queue::get_sender();
         let guard = s.lock();
         let sender = guard.expect("get sender fail."); // crash here if the channel is malfunc
@@ -42,7 +43,7 @@ fn handle_ocr_event(event: &GithubEvent, comment: &str) -> Result<(), anyhow::Er
 
 fn handle_time_spent_event(comment: &str, event: &GithubEvent) -> Result<()> {
     if let Some(tp) = time_spent::get_time_spent(comment, event, None) {
-        let msg = events::msg::Message::TimeSpent(tp);
+        let msg = Message::TimeSpent(tp);
         let s = queue::get_sender();
         let guard = s.lock();
         let sender = guard.expect("get sender fail."); // crash here if the channel is malfunc
