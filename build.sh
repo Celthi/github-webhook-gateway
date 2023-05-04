d87034c356c85c6df5adcfcb94683a9d22cc8896
celthi <evan_l00@qq.com>
2023-05-04 19:54:49 +0800

    bump version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=2.20.3
+version=2.20.4
 cargo check && docker build . -t webhook_gateway:$version
 sleep 1
 kubectl set image  deployment/webhook-gateway webhook-gateway=webhook_gateway:$version
