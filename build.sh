85b30b793d71ebb25ca84ce0c6e7c1ee71339e38
EvanL00 <evan_l00@qq.com>
2023-09-30 10:37:23 +0800

    network host docker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version=2.22.0
-cargo check && docker build . -t webhook_gateway:$version
+cargo check && docker build . --network=host -t webhook_gateway:$version
 sleep 1
 kubectl set image  deployment/webhook-gateway webhook-gateway=webhook_gateway:$version
