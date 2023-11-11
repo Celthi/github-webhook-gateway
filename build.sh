4d3ad5ff8225fea08493adcf1fc280fecd654c40
EvanL00 <evan_l00@qq.com>
2023-11-11 15:46:57 +0800

    refactor

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=2.22.6
+version=2.22.7
 cargo check && docker build . --network=host -t webhook_gateway:$version
 sleep 1
 if [[ $? -ne 0 ]]; then
--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,10 @@
 use crate::reg;
 
-static KEYWORDS: [&str; 2] = ["XT ABAs", "XT I'm running ABAs"];
 
 pub fn contain_keywords(s: &str) -> bool {
-    contains_ocr_patten(s) || contains_time_spent_pattern(s)
+    let pat = reg!(r"(XT ABAs)|(XT I'm running ABAs)|((T|t)hanks\s(?P<t>(\d{1})|(\d\.\d{1,3}))!)");
+    pat.is_match(s)
 }
-
 pub fn contains_time_spent_pattern(s: &str) -> bool {
     let pat = reg!(r"(T|t)hanks\s(?P<t>(\d{1})|(\d\.\d{1,3}))!");
     pat.captures(s).is_some()
@@ -16,7 +15,8 @@ pub fn contains_rally_pattern(s: &str) -> bool {
 }
 
 pub fn contains_ocr_patten(s: &str) -> bool {
-    KEYWORDS.iter().any(|k| s.contains(k))
+    let pat = reg!(r"(XT ABAs)|(XT I'm running ABAs)");
+    pat.is_match(s)
 }
 
 #[cfg(test)]
@@ -30,5 +30,12 @@ mod test {
         assert!(contains_time_spent_pattern(s));
         let s = "hi 26803fda-2e78-4d9f-931d-84b8261d6f7b";
         assert!(contains_rally_pattern(s));
+        assert!(contains_ocr_patten("XT ABAs"));
+        assert!(contains_ocr_patten("XT I'm running ABAs:"));
+        assert!(contain_keywords("XT ABAs"));
+        assert!(contain_keywords("XT I'm running ABAs: Hi"));
+        assert!(contain_keywords("Thanks 3!"));
+        assert!(contain_keywords("Thanks 0.4!"));
+
     }
 }
